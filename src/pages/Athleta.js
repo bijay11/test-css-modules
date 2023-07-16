a6a9b3e37fde9f6ddd465d9c75ca8e3cae309191
Bijay Tamang <tmg.bijay11@gmail.com>
2023-07-16 13:04:01 -0400

    athleta changes

--- src/pages/Athleta.js
+++ src/pages/Athleta.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Button } from "../components/Button/Button";
 import styles from "./Athleta.modules.scss";
+import { Container } from "../components/Container";
 
 export const Athleta = () => {
   return (
     <div className={styles.athleta}>
-      <div>Athleta</div>
-      <Button text="Primary Button" type="primary" />
-      <Button text="Secondary  Button" type="secondary" />
+      <Container>
+        <div>Athleta</div>
+        <Button text="Primary Button" type="primary" />
+        <Button text="Secondary  Button" type="secondary" />
+      </Container>
     </div>
   );
 };
