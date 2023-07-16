e6015529503f05d1730fe561e14e59ee1b97ffef
Bijay Tamang <tmg.bijay11@gmail.com>
2023-07-16 09:53:19 -0400

    css changes

--- src/Root.js
+++ src/Root.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./components/Navbar/Navbar";
+// import styles from "./Root.modules.scss";
 
 export const Root = () => {
   return (
-    <>
+    <div>
       <Navbar />
       <Outlet />
-    </>
+    </div>
   );
 };
