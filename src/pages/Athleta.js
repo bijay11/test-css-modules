3562b293eb8f36102b47daff0cfa1d57d2c7143b
Bijay Tamang <tmg.bijay11@gmail.com>
2023-07-16 11:54:46 -0400

    css

--- src/pages/Athleta.js
+++ src/pages/Athleta.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Button } from "../components/Button/Button";
+import styles from "./Athleta.modules.scss";
 
 export const Athleta = () => {
   return (
-    <>
+    <div className={styles.athleta}>
       <div>Athleta</div>
       <Button text="Primary Button" type="primary" />
       <Button text="Secondary  Button" type="secondary" />
-    </>
+    </div>
   );
 };
