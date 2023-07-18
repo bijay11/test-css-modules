6dea315277d9842993d2276f82d7c651475d8095
Bijay Tamang <tmg.bijay11@gmail.com>
2023-07-17 22:32:53 -0400

    refactor

--- src/Root.js
+++ src/Root.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./components/Navbar/Navbar";
-import styles from "./Root.modules.scss";
+import "./Root.scss";
 
 export const Root = () => {
   return (
-    <div style={styles.app}>
+    <>
       <Navbar />
       <Outlet />
-    </div>
+    </>
   );
 };
