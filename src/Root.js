5ed84565031ceeba6661278f453f0b797be3db60
Bijay Tamang <tmg.bijay11@gmail.com>
2023-07-22 17:56:47 -0400

    feature flag example

--- src/Root.js
+++ src/Root.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./components/Navbar/Navbar";
 import "./Root.scss";
 
 export const Root = () => {
+  //
+  // this is jsut a demo
+  // if FF is on, inject these variables from server-side
+  //
+  const featureFlag = true;
+  useEffect(() => {
+    if (featureFlag) {
+      const brFFColor = getComputedStyle(document.documentElement)
+        .getPropertyValue("--slight-orange-color")
+        .trim();
+
+      //
+      // we could just inject color code instead of `brFFColor`, adding it just incase we want to utitlize the predefined colors.
+      //
+      document.documentElement.style.setProperty("--ff-br-bg-color", brFFColor);
+    }
+    // --ff-br-bg-color: var(--slight-orange-color);
+    //  var(--slight-orange-color);
+  }, []);
   return (
     <>
       <Navbar />
