5e538b68f25f3001768ecda6b52726827dd6f108
Bijay Tamang <tmg.bijay11@gmail.com>
2023-07-27 09:50:27 -0400

    create util function to extract classNames

--- src/core-ui/utils/getCssClassNames.js
+++ src/core-ui/utils/getCssClassNames.js
@@ -1,9 +1,9 @@
 /**
  * Takes an array of classnames and returns a new array with each classname prepended by a space.
- * If a classname is falsy (e.g., empty string, undefined, null), it will be excluded from the returned array.
+ * If a classname is falsy (e.g., empty string, undefined, null), it will be considered empty string.
  *
  * @param {Array} customClassnames - An array of classnames.
- * @returns {Array} - A new array with classnames, each prepended by a space.
+ * @returns {Array} - A new array with classnames, each prepended by a space or just space.
  */
 export const getCssClassNames = (customClassnames) =>
   customClassnames.map((customClassname) =>
